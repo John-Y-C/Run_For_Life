7cbd8e2a1c28be18524c5d46a65f005cc4345f39
John-Y-C <Y_John1214@163.com>
2016-09-02 17:25:09 +0800

    页面已用fragment分好

---
+++ app/src/main/java/com/example/administrator/fragment/FragmentHealth.java
@@ -0,0 +1,29 @@
+package com.example.administrator.fragment;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.administrator.runforlife.R;
+
+/**
+ * Created by Administrator on 2016/9/2/002.
+ */
+public class FragmentHealth extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View inflate = View.inflate(getActivity(), R.layout.fragment_health, null);
+        TextView tv_fragment = (TextView) inflate.findViewById(R.id.testView);
+        tv_fragment.setText("Health");
+        tv_fragment.setTextColor(Color.RED);
+        return inflate;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/runforlife/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.administrator.runforlife;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.administrator.fragment.FragmentDate;
+import com.example.administrator.fragment.FragmentHealth;
+import com.example.administrator.fragment.FragmentRun;
+import com.example.administrator.fragment.FragmentSetting;
+import com.example.administrator.fragment.FragmentWeather;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.fragmentlayout)
+    FrameLayout fragmentlayout;
+    @InjectView(R.id.rb_fragment_run)
+    RadioButton rbFragmentRun;
+    @InjectView(R.id.rb_fragment_weather)
+    RadioButton rbFragmentWeather;
+    @InjectView(R.id.rb_fragment_health)
+    RadioButton rbFragmentHealth;
+    @InjectView(R.id.rb_fragment_date)
+    RadioButton rbFragmentDate;
+    @InjectView(R.id.rb_fragment_setting)
+    RadioButton rbFragmentSetting;
+    @InjectView(R.id.rg_framgentcontent_bottom)
+    RadioGroup rgFramgentcontentBottom;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        fragmentManager = getFragmentManager();
+    }
+
+    @OnClick({R.id.rb_fragment_run, R.id.rb_fragment_weather, R.id.rb_fragment_health, R.id.rb_fragment_date, R.id.rb_fragment_setting})
+    public void onClick(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (view.getId()) {
+            case R.id.rb_fragment_run:
+                fragmentTransaction.replace(R.id.fragmentlayout,new FragmentRun());
+                fragmentTransaction.commit();
+                break;
+            case R.id.rb_fragment_weather:
+                fragmentTransaction.replace(R.id.fragmentlayout,new FragmentWeather());
+                fragmentTransaction.commit();
+                break;
+            case R.id.rb_fragment_health:
+                fragmentTransaction.replace(R.id.fragmentlayout,new FragmentHealth());
+                fragmentTransaction.commit();
+                break;
+            case R.id.rb_fragment_date:
+                fragmentTransaction.replace(R.id.fragmentlayout,new FragmentDate());
+                fragmentTransaction.commit();
+                break;
+            case R.id.rb_fragment_setting:
+                fragmentTransaction.replace(R.id.fragmentlayout,new FragmentSetting());
+                fragmentTransaction.commit();
+                break;
+        }
+    }
+}
